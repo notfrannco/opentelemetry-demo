bf4a653e322ddd4adae21ccc562970db4afe5d6c
Your Name <you@example.com>
2024-03-15 09:24:34 -0300

    first

---
+++ src/quoteservice/public/index.php
@@ -0,0 +1,79 @@
+<?php
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+
+
+declare(strict_types=1);
+
+use DI\Bridge\Slim\Bridge;
+use DI\ContainerBuilder;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use OpenTelemetry\API\Common\Log\LoggerHolder;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Log\LogLevel;
+use React\EventLoop\Loop;
+use React\Http\HttpServer;
+use React\Socket\SocketServer;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+LoggerHolder::set(
+    new Logger('otel-php', [new StreamHandler('php://stdout', LogLevel::DEBUG)])
+);
+
+// Instantiate PHP-DI ContainerBuilder
+$containerBuilder = new ContainerBuilder();
+
+// Set up settings
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($containerBuilder);
+
+// Set up dependencies
+$dependencies = require __DIR__ . '/../app/dependencies.php';
+$dependencies($containerBuilder);
+
+// Build PHP-DI Container instance
+$container = $containerBuilder->build();
+
+// Instantiate the app
+AppFactory::setContainer($container);
+$app = Bridge::create($container);
+
+// Register middleware
+$app->addRoutingMiddleware();
+
+// Register routes
+$routes = require __DIR__ . '/../app/routes.php';
+$routes($app);
+
+// Add Body Parsing Middleware
+$app->addBodyParsingMiddleware();
+
+// Add Error Middleware
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+Loop::get()->addSignal(SIGTERM, function() {
+    exit;
+});
+
+$server = new HttpServer(function (ServerRequestInterface $request) use ($app) {
+    $response = $app->handle($request);
+    echo sprintf('[%s] "%s %s HTTP/%s" %d %d %s',
+        date('Y-m-d H:i:sP'),
+        $request->getMethod(),
+        $request->getUri()->getPath(),
+        $request->getProtocolVersion(),
+        $response->getStatusCode(),
+        $response->getBody()->getSize(),
+        PHP_EOL,
+    );
+
+    return $response;
+});
+$address = '0.0.0.0:' . getenv('QUOTE_SERVICE_PORT');
+$socket = new SocketServer($address);
+$server->listen($socket);
+
+echo "Listening on: {$address}" . PHP_EOL;
